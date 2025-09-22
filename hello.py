9b8c5c9e8cf76d52215554c21a24d1efaceaecc5
Moksh5311 <moksh.mehta@mail.utoronto.ca>
2025-09-22 11:20:40 -0400

    Figured out Act 1.3 for regular page, will add it t to user page now

--- hello.py
+++ hello.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
-
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    #return '<h1> Hello World! </h1>'
-    return render_template('index.html')
+    current_time = datetime.utcnow()  # naive UTC datetime
+
+    #current_time = now.astimezone()
+    print("Current time passed:", current_time)
+    return render_template('index.html', current_time=current_time)
 
 @app.route('/user/<name>')
 def user(name):
-    #return '<h1> Hello {}! </h1>'.format(name)
-    return render_template('user.html', name = name)
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
